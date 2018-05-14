a22a047c87ed0036e507acd364860d0109fd7d58
Anderson Laurentino <alaurentino.br@gmail.com>
2018-05-14 20:19:14 -0300

    Add nrf24.py

--- init.py
+++ init.py
@@ -1,56 +1,10 @@
-from RPi.GPIO as GPIO
-from lib_nrf24/lib_nrf24 import NRF24
 import time
-import spidev
 import requests
-
+from nrf24 import sendData
 
 SERVER_PORT = "8080"
 SERVER_HOST = "http://ec2-18-204-229-11.compute-1.amazonaws.com:{0}".format(SERVER_PORT)
 TOKEN = ""
-
-
-GPIO.setmode(GPIO.BCM)
-
-pipes = [[0xE8, 0XE8, 0xF0, 0xF0, 0xE1], [0xF0, 0xF0, 0xF0, 0xF0, 0xE1]]
-
-radio = NRF24(GPIO, spidev.SpiDev())
-radio.begin(0, 17)
-
-radio.setPayloadSize(32)
-radio.setChannel(0x76)
-radio.setDataRate(NRF24.BR_1MBPS)
-radio.setPALevel(NRF24.PA_MIN)
-
-radio.setAutoAck(True)
-radio.enableDynamicPayloads()
-radio.enableAckPayload()
-
-radio.openWritingPipe(pipes[0])
-radio.openReadingPip(1, pipes[1])
-radio.printDetails()
-#radio.startListening()
-
-def sendData(message):
-    message = list("message")
-    
-    while len(message) < 32:
-        message.append(0)
-    
-    start = time.time()
-    radio.write(message)
-    print("Sending message: {}".format(message))
-    
-    radio.startListening()
-
-    while not radio.available(0) and time.time() - start <= 5:
-        time.sleep(1/100)
-    
-    receivedMessage = []
-    radio.read(receivedMessage, radio.getDynamicPayloadSize())
-
-    print("Received: {}".format(receivedMessage))
-
 
 def login(email, password):
     payload = {'email': email, 'password': password}
