f0c4f5646af2068be1922cccd8cca011521767c1
Anderson Laurentino <alaurentino.br@gmail.com>
2018-05-19 23:27:08 -0300

    Remove all HTTP protocol

--- update_schedule.py
+++ update_schedule.py
@@ -2,7 +2,6 @@ import time
 import json
 import paho.mqtt.client as mqtt
 from database import database, saveDatabase
-# from api import login, getEnvironments, getSchedulesByUUID
 
 def on_connect(client, userdata, flags, rc):
     print("MQTT> Connected")
@@ -26,28 +25,3 @@ def update_schedules():
     client.username_pw_set(database["mqtt_user"], database["mqtt_password"])
 
     client.loop_forever()
-
-# def update_schedules():
-#     token = login(database["email"], database["password"]).json()["token"]
-
-#     while 1:
-#         try:
-#             environments = getEnvironments(token).json()
-
-#             if type(environments) == type({"name": "name"}):
-#                 if environments["code"] == 1:
-#                     token = login(database["email"], database["password"]).json()["token"]
-#                     environments = getEnvironments(token).json()
-
-#             database["environments"] = []
-#             if len(environments) > 0:
-#                 for env in environments:
-#                     schedule = getSchedulesByUUID(token, env["uuid"]).json()
-#                     database["environments"].append({"uuid": env["uuid"], "schedule": schedule})
-            
-#             print("> Saved last schedules")
-#             saveDatabase()
-#             time.sleep(10)
-#         except Exception as e:
-#             print("\nConnetion Refused!\nWait 30 seconds\n")
-#             time.sleep(30)
