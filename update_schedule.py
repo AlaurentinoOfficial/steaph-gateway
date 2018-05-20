65b7a9f668d3b958aba2889d704fe81b4faae177
Anderson Laurentino <alaurentino.br@gmail.com>
2018-05-19 23:51:16 -0300

    Send environmnets status by MQTT

--- update_schedule.py
+++ update_schedule.py
@@ -10,10 +10,13 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     if msg.topic == "/steaph/things/" + database["public_key"] + "/environments":
         try:
-            database["environments"] = json.load(msg.payload)
+            database["environments"] = json.loads(msg.payload)
             saveDatabase()
             print("MQTT> Environments updated successfully!")
-        except:
+
+            client.publish("/steaph/things/" + database["public_key"] + "/environments/status", payload=json.dumps(database["environments_status"]), qos=1)
+        except Exception as e:
+            print(e)
             print("MQTT> Invalid payload from " + msg.topic)
 
 def update_schedules():
@@ -21,7 +24,7 @@ def update_schedules():
     client.on_connect = on_connect
     client.on_message = on_message
 
-    client.connect(database["mqtt_server"], database["mqtt_port"], 60)
     client.username_pw_set(database["mqtt_user"], database["mqtt_password"])
+    client.connect(database["mqtt_server"], database["mqtt_port"], 60)
 
     client.loop_forever()
