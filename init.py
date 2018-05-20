4146197ed03c9323ce1a4fd2a6cd71f056a02241
Anderson Laurentino <alaurentino.br@gmail.com>
2018-05-19 23:25:50 -0300

    Updated api communication to MQTT

--- init.py
+++ init.py
@@ -1,30 +1,6 @@
 import threading
 from update_thing import update_things
 from update_schedule import update_schedules
-import sys, os
-
-def wait_key():
-    result = None
-    if os.name == 'nt':
-        import msvcrt
-        result = msvcrt.getch()
-    else:
-        import termios
-        fd = sys.stdin.fileno()
-
-        oldterm = termios.tcgetattr(fd)
-        newattr = termios.tcgetattr(fd)
-        newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
-        termios.tcsetattr(fd, termios.TCSANOW, newattr)
-
-        try:
-            result = sys.stdin.read(1)
-        except IOError:
-            pass
-        finally:
-            termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
-
-    return result
 
 if __name__ == "__main__":
     updateSchedules = threading.Thread(target=update_schedules)
--- update_schedule.py
+++ update_schedule.py
@@ -1,28 +1,53 @@
 import time
+import json
+import paho.mqtt.client as mqtt
 from database import database, saveDatabase
-from api import login, getEnvironments, getSchedulesByUUID
+# from api import login, getEnvironments, getSchedulesByUUID
+
+def on_connect(client, userdata, flags, rc):
+    print("MQTT> Connected")
+    client.subscribe("/steaph/things/" + database["public_key"] + "/environments", 1)
+
+def on_message(client, userdata, msg):
+    if msg.topic == "/steaph/things/" + database["public_key"] + "/environments":
+        try:
+            database["environments"] = json.load(msg.payload)
+            saveDatabase()
+            print("MQTT> Environments updated successfully!")
+        except:
+            print("MQTT> Invalid payload from " + msg.topic)
 
 def update_schedules():
-    token = login(database["email"], database["password"]).json()["token"]
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
 
-    while 1:
-        try:
-            environments = getEnvironments(token).json()
+    client.connect(database["mqtt_server"], database["mqtt_port"], 60)
+    client.username_pw_set(database["mqtt_user"], database["mqtt_password"])
 
-            if type(environments) == type({"name": "name"}):
-                if environments["code"] == 1:
-                    token = login(database["email"], database["password"]).json()["token"]
-                    environments = getEnvironments(token).json()
+    client.loop_forever()
 
-            database["environments"] = []
-            if len(environments) > 0:
-                for env in environments:
-                    schedule = getSchedulesByUUID(token, env["uuid"]).json()
-                    database["environments"].append({"uuid": env["uuid"], "schedule": schedule})
+# def update_schedules():
+#     token = login(database["email"], database["password"]).json()["token"]
+
+#     while 1:
+#         try:
+#             environments = getEnvironments(token).json()
+
+#             if type(environments) == type({"name": "name"}):
+#                 if environments["code"] == 1:
+#                     token = login(database["email"], database["password"]).json()["token"]
+#                     environments = getEnvironments(token).json()
+
+#             database["environments"] = []
+#             if len(environments) > 0:
+#                 for env in environments:
+#                     schedule = getSchedulesByUUID(token, env["uuid"]).json()
+#                     database["environments"].append({"uuid": env["uuid"], "schedule": schedule})
             
-            print("> Saved last schedules")
-            saveDatabase()
-            time.sleep(10)
-        except Exception as e:
-            print("\nConnetion Refused!\nWait 30 seconds\n")
-            time.sleep(30)
+#             print("> Saved last schedules")
+#             saveDatabase()
+#             time.sleep(10)
+#         except Exception as e:
+#             print("\nConnetion Refused!\nWait 30 seconds\n")
+#             time.sleep(30)
